5a49ee63cd7e6f4ae4afee9307f2ede03c4d4673
Thibaut Nazare <tnazare@gmail.com>
2014-09-21 20:25:20 +0200

    Add basic support for form generation. Need to improuve support for resource that contains resources (cladimed)

--- app/scripts/controllers/wadlparser.js
+++ app/scripts/controllers/wadlparser.js
@@ -7,51 +7,93 @@
  */
 angular.module('wadlFormApp')
   .controller('WadlParser', function ($scope, $http) {
-    $scope.apiHttpUrl = '';
-    $scope.urlBack = 'not working';
+    $scope.urlBack = 'waiting...';
+    $scope.numbersTypes = ['xs:int'];
+    $scope.stringsTypes = ['xs:string'];
+    $scope.booleansTypes = ['xs:boolean'];
+
+    $scope.resources = {};
+    $scope.selectedResource = {};
+    $scope.selectedMethod = {};
     $scope.submit = function(){
         $scope.urlBack = 'It\'s working!!!!';
     };
 
-    $scope.loadWadl = function(apiUrl){
-        console.log('apiUrl = '+apiUrl);
-        $http.get(apiUrl)
+    $scope.onClick = function(url, event){
+      $scope.resources  = {};
+      var httpResponsePromise = $http.get(url)
         .success(function(data,status,header,config) {
-            console.log(status);
-            console.log(header);
-            console.log(config);
-            var resourcesXml = jQuery(data).find('resource');
-            var resources = {};
-            resources.innerArray = [];
-            resources.base = jQuery(data).find('resources').attr('base');
-            resourcesXml.each(function(){
-                resources.innerArray.push(this);
-            }); 
-            for (var i = 0; i < resources.innerArray.length; i++) {
-                resources.innerArray[i].methods = [];
-                resources.innerArray[i].path = jQuery(resources.innerArray[i]).attr('path');
-                jQuery(resources.innerArray[i]).find('method').each(function(){
-                    resources.innerArray[i].methods.push(this);
-                }); 
-                for (var j = 0; j < resources.innerArray[i].methods.length; j++) {
-                    resources.innerArray[i].methods[j].params = [];
-                    resources.innerArray[i].methods[j].httpMethodName = jQuery(resources.innerArray[i].methods[j]).attr('name');
-                    jQuery(resources.innerArray[i].methods[j]).find('param').each(function(){
-                        var param = {};
-                        param.httpStyle = jQuery(this).attr('style');
-                        param.httpType = jQuery(this).attr('type');
-                        param.httpName = jQuery(this).attr('name');
-                        param.httpDefault = jQuery(this).attr('default');
-                        param.xmlNamespaceSchema = jQuery(this).attr('xmlns:xs');
-                        resources.innerArray[i].methods[j].params.push(param);
-                    }); 
-                }
-            }
-            $scope.resources = resources;
+            $scope.urlBack = 'loading wadl...';
+            $scope.resources = $scope.parseWadl(data);
+            $scope.urlBack = 'wadl loaded';    
         })
         .error(function(data,status,header,config) {
-           console.error('error occured!');
-           console.error('status = '+status)
+            $scope.urlBack = 'Error';
         });
+        
     };
+
+
+    $scope.parseWadl = function(xmlString){
+        var resourcesXml = jQuery(xmlString).find('resource');
+        var resources = {};
+        resources.innerArray = [];
+        resources.base = jQuery(xmlString).find('resources').attr('base');
+        resourcesXml.each(function(){
+            resources.innerArray.push(this);
+        }); 
+        for (var i = 0; i < resources.innerArray.length; i++) {
+            resources.innerArray[i].methods = [];
+            resources.innerArray[i].path = jQuery(resources.innerArray[i]).attr('path');
+            jQuery(resources.innerArray[i]).find('method').each(function(){
+                resources.innerArray[i].methods.push(this);
+            }); 
+            for (var j = 0; j < resources.innerArray[i].methods.length; j++) {
+                resources.innerArray[i].methods[j].params = [];
+                resources.innerArray[i].methods[j].httpMethodName = jQuery(resources.innerArray[i].methods[j]).attr('name');
+                jQuery(resources.innerArray[i].methods[j]).find('param').each(function(){
+                    var param = {};
+                    param.httpStyle = jQuery(this).attr('style');
+                    param.httpType = jQuery(this).attr('type');
+                    param.httpName = jQuery(this).attr('name');
+                    param.httpDefault = jQuery(this).attr('default');
+                    param.xmlNamespaceSchema = jQuery(this).attr('xmlns:xs');
+                    resources.innerArray[i].methods[j].params.push(param);
+                }); 
+            }
+        }
+        return resources;        
+    };
+
+    $scope.isNumber = function (param){
+        if(typeof param == 'undefined')
+            return false;
+        for(var i = 0 ; i < $scope.numbersTypes.length ; i ++){
+            if(typeof param.httpType != 'undefined' && $scope.numbersTypes[i] == param.httpType){
+                return true;
+            }
+        }
+    };
+
+    $scope.isString = function (param){
+        if(typeof param == 'undefined')
+            return false;
+        console.log("isString; param : "+param);
+        for(var i = 0 ; i < $scope.stringsTypes.length ; i ++){
+            if(typeof param.httpType != 'undefined' && $scope.stringsTypes[i] == param.httpType){
+                return true;
+            }
+        }
+    };
+
+    $scope.isBoolean = function (param){
+        if(typeof param == 'undefined')
+            return false;
+        for(var i = 0 ; i < $scope.booleansTypes.length ; i ++){
+            if(typeof param.httpType != 'undefined' && $scope.booleansTypes[i] == param.httpType){
+                return true;
+            }
+        }
+    };
+
   });
--- test/spec/controllers/wadlparser.js
+++ test/spec/controllers/wadlparser.js
@@ -4,22 +4,31 @@ describe('WadlParser controller tests', function() {
     var WadlParser,
     scope,
     httpBackend,
-    http;
+    http,
+    xmlString;
 
     beforeEach(module('wadlFormApp'));
 
     beforeEach(inject(function($rootScope, $controller, $httpBackend, $http) {
       scope = $rootScope.$new();
       httpBackend = $httpBackend;
-      httpBackend.when('GET', 'wadlFiles/application1.wadl').respond('<?xml version="1.0" encoding="UTF-8" standalone="yes"?><application xmlns="http://wadl.dev.java.net/2009/02"><doc xmlns:jersey="http://jersey.java.net/" jersey:generatedBy="Jersey: 1.13 06/29/2012 05:14 PM"/><grammars><include href="application.wadl/xsd0.xsd"><doc xml:lang="en" title="Generated"/></include></grammars><resources base="http://dev-software.vidal.net/excalibur-rest-snapshot/rest/"><resource path="/pmsi/scored-indication-groups"><method name="GET" id="computeScores"><request><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="codes"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="productId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="vmpId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="packId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="ucdId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="pathologies"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:int" style="query" name="selectedId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="10" type="xs:int" style="query" name="scoreBalancing"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="LOW" type="xs:string" style="query" name="rejectNoise"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="25" type="xs:int" style="query" name="discriminantCursor"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="30" type="xs:int" style="query" name="depreciationRate"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="false" type="xs:boolean" style="query" name="verbose"/></request><response><representation mediaType="application/atom+xml"/></response></method><method name="POST" id="computeScores"><request><ns2:representation xmlns:ns2="http://wadl.dev.java.net/2009/02" xmlns="" mediaType="text/xml" element="request"/></request><response><representation mediaType="application/atom+xml"/></response></method></resource></resources></application>');
-      http = $http
+      httpBackend.when('GET', 'wadlFiles/application1.wadl').respond(xmlString);
+      http = $http;
+      xmlString = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><application xmlns="http://wadl.dev.java.net/2009/02"><doc xmlns:jersey="http://jersey.java.net/" jersey:generatedBy="Jersey: 1.13 06/29/2012 05:14 PM"/><grammars><include href="application.wadl/xsd0.xsd"><doc xml:lang="en" title="Generated"/></include></grammars><resources base="http://dev-software.vidal.net/excalibur-rest-snapshot/rest/"><resource path="/pmsi/scored-indication-groups"><method name="GET" id="computeScores"><request><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="codes"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="productId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="vmpId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="packId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="ucdId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" style="query" name="pathologies"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:int" style="query" name="selectedId"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="10" type="xs:int" style="query" name="scoreBalancing"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="LOW" type="xs:string" style="query" name="rejectNoise"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="25" type="xs:int" style="query" name="discriminantCursor"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="30" type="xs:int" style="query" name="depreciationRate"/><param xmlns:xs="http://www.w3.org/2001/XMLSchema" default="false" type="xs:boolean" style="query" name="verbose"/></request><response><representation mediaType="application/atom+xml"/></response></method><method name="POST" id="computeScores"><request><ns2:representation xmlns:ns2="http://wadl.dev.java.net/2009/02" xmlns="" mediaType="text/xml" element="request"/></request><response><representation mediaType="application/atom+xml"/></response></method></resource></resources></application>';
       WadlParser = $controller('WadlParser', {$scope: scope, $http: http});
     }));
-
-    it('should parse xml input string', function () {
+  
+    it('should get Wadl xml from url and set xml as a string in wadlXmlAsString', function () {   
       httpBackend.expectGET('wadlFiles/application1.wadl');
-      scope.loadWadl('wadlFiles/application1.wadl');
-      httpBackend.flush();
+      scope.onClick('wadlFiles/application1.wadl', null);
+      httpBackend.flush();  
+      expect(scope.resources).not.toBe(null);
+    });
+
+
+    /*
+    it('should parse xml input string', function () {
+      scope.parseWadl(xmlString);
       expect(scope.xmlDoc).not.toBe(null);
     });
 
@@ -50,6 +59,11 @@ describe('WadlParser controller tests', function() {
       expect(scope.resources.innerArray[0].methods[0].params[7].httpStyle).toBe('query');
       expect(scope.resources.innerArray[0].methods[0].params[7].xmlNamespaceSchema).toBe('http://www.w3.org/2001/XMLSchema');
     });
+
+    it('should parse xml and retrive params within the first request of the first resource', function () {
+
+    }); */
+
   });
   
 });
