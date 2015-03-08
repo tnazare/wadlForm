ae3e7676b6c591bef444a410a301d7b491f5a5f1
Thibaut Nazare <tnazare@gmail.com>
2015-03-08 16:50:42 +0100

    change test drug

--- test/spec/controllers/wadlparser.js
+++ test/spec/controllers/wadlparser.js
@@ -192,50 +192,62 @@ describe('WadlParser controller tests', function() {
     });
 
     it('should parse xml response and extract entry infos' , function () {
-        var response = '<?xml version="1.0" encoding="UTF-8"?> <feed xmlns="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:vidal="http://api.vidal.net/-/spec/vidal-api/1.0/" xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/"> <title>Search Products - Query :bedel</title> <link rel="self" type="application/atom+xml" href="/rest/api/products?q=bedel&start-page=1&page-size=25" /> <id>/rest/api/products?q=bedel&start-page=1&page-size=25</id> <updated>2014-09-17T22:00:00Z</updated> <dc:date>2014-09-17T22:00:00Z</dc:date> <opensearch:itemsPerPage>25</opensearch:itemsPerPage> <opensearch:totalResults>1</opensearch:totalResults> <opensearch:startIndex>1</opensearch:startIndex> <entry> <title>BEDELIX 3 g pdre p susp buv</title> <link rel="alternate" type="application/atom+xml" href="/rest/api/product/1971" /> <link rel="related" type="application/atom+xml" href="/rest/api/product/1971/packages" title="PACKAGES" /> <link rel="related" type="application/atom+xml" href="/rest/api/product/1971/documents/opt" title="OPT_DOCUMENT" /> <link rel="related" type="application/atom+xml" href="/rest/api/product/1971/documents" title="DOCUMENTS" /> <link rel="related" type="application/atom+xml" href="/rest/api/vmp/8028" title="VMP" /> <category term="PRODUCT" /> <author> <name>VIDAL</name> </author> <id>vidal://product/1971</id> <updated>2014-09-17T22:00:00Z</updated> <summary type="text">BEDELIX 3 g pdre p susp buv</summary> <vidal:id>1971</vidal:id> <vidal:dispensationPlace name="PHARMACY">PHARMACY</vidal:dispensationPlace> <vidal:activePrinciples>montmorillonite beidellitique</vidal:activePrinciples> <vidal:horsGHS>false</vidal:horsGHS> <vidal:refundRate name="NR">NR</vidal:refundRate> <vidal:itemType name="VIDAL">VIDAL</vidal:itemType> <vidal:marketStatus name="AVAILABLE">Commercialisé</vidal:marketStatus> <vidal:company type="OWNER" vidalId="2908">Ipsen Pharma</vidal:company> <vidal:drugInSport>false</vidal:drugInSport> <vidal:exceptional>false</vidal:exceptional> <vidal:retrocession>false</vidal:retrocession> <vidal:beCareful>false</vidal:beCareful> <vidal:midwife>true</vidal:midwife> <vidal:perVolume>3g</vidal:perVolume> <vidal:hasPublishedDoc>true</vidal:hasPublishedDoc> <vidal:onMarketDate format="yyyy-MM-dd">1980-03-15</vidal:onMarketDate> <vidal:withoutPrescription>true</vidal:withoutPrescription> <vidal:vmp vidalId="8028">montmorillonite beidellitique * 3 g ; voie orale + voie rectale ; pdre p susp buv/rect</vidal:vmp> </entry> </feed>';
+        var response = '<?xml version="1.0" encoding="UTF-8"?> <feed xmlns="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:vidal="http://api.vidal.net/-/spec/vidal-api/1.0/" xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/"> <title>Search Products - Query :augmentin</title> <link rel="self" type="application/atom+xml" href="/rest/api/products?q=augmentin&amp;start-page=1&amp;page-size=25" /> <id>/rest/api/products?q=augmentin&amp;start-page=1&amp;page-size=25</id> <updated>2048-07-21T22:00:00Z</updated> <dc:date>2048-07-21T22:00:00Z</dc:date> <opensearch:itemsPerPage>25</opensearch:itemsPerPage> <opensearch:totalResults>10</opensearch:totalResults> <opensearch:startIndex>1</opensearch:startIndex> <entry> <title>AUGMENTIN 100 mg/12,5 mg p ml pdre p susp buv Enf</title> <link rel="alternate" type="application/atom+xml" href="/rest/api/product/1735" /> <link rel="related" type="application/atom+xml" href="/rest/api/product/1735/packages" title="PACKAGES" /> <link rel="related" type="application/atom+xml" href="/rest/api/product/1735/documents/opt?patient=false" title="OPT_DOCUMENT" /> <link rel="related" type="application/atom+xml" href="/rest/api/product/1735/documents" title="DOCUMENTS" /> <link rel="related" type="application/atom+xml" href="/rest/api/vmp/2675" title="VMP" /> <category term="PRODUCT" /> <author> <name>VIDAL</name> </author> <id>vidal://product/1735</id> <updated>2048-07-21T22:00:00Z</updated> <summary type="text">AUGMENTIN 100 mg/12,5 mg p ml pdre p susp buv Enf</summary> <vidal:id>1735</vidal:id> <vidal:dispensationPlace name="PHARMACY">PHARMACY</vidal:dispensationPlace> <vidal:activePrinciples>acide clavulanique sel de K; amoxicilline trihydrate</vidal:activePrinciples> <vidal:horsGHS>false</vidal:horsGHS> <vidal:genericType name="REFERENT">RÃ©fÃ©rent</vidal:genericType> <vidal:list name="I">Liste 1</vidal:list> <vidal:refundRate name="_65">65%</vidal:refundRate> <vidal:itemType name="VIDAL">VIDAL</vidal:itemType> <vidal:marketStatus name="AVAILABLE">CommercialisÃ©</vidal:marketStatus> <vidal:company type="OWNER" vidalId="986">GlaxoSmithKline</vidal:company> <vidal:drugInSport>false</vidal:drugInSport> <vidal:exceptional>false</vidal:exceptional> <vidal:retrocession>false</vidal:retrocession> <vidal:beCareful>false</vidal:beCareful> <vidal:midwife>false</vidal:midwife> <vidal:perVolume>100mg/12,5mgpml</vidal:perVolume> <vidal:hasPublishedDoc>true</vidal:hasPublishedDoc> <vidal:onMarketDate format="yyyy-MM-dd">1998-05-14</vidal:onMarketDate> <vidal:withoutPrescription>false</vidal:withoutPrescription> <vidal:vmp vidalId="2675">acide clavulanique (sel de K) * 12,5 mg/ml + amoxicilline * 100 mg/ml ; voie orale ; pdre p susp buv</vidal:vmp> </entry> </feed>';
 
         var entry = scope.extractEntry(jQuery(response).children('entry'));
 
         expect(entry).not.toBe(null);
-        expect(entry.title.innerText).toBe("BEDELIX 3 g pdre p susp buv");
+        expect(entry.title.innerText).toBe("AUGMENTIN 100 mg/12,5 mg p ml pdre p susp buv Enf");
         expect(entry.links.length).toBe(5);
         expect(entry.links[0].rel).toBe("alternate");
         expect(entry.links[0].type).toBe("application/atom+xml");
-        expect(entry.links[0].href).toBe("/rest/api/product/1971");
+        expect(entry.links[0].href).toBe("/rest/api/product/1735");
 
         expect(entry.links[1].rel).toBe("related");
         expect(entry.links[1].type).toBe("application/atom+xml");
-        expect(entry.links[1].href).toBe("/rest/api/product/1971/packages");
+        expect(entry.links[1].href).toBe("/rest/api/product/1735/packages");
 
         expect(entry.links[2].rel).toBe("related");
         expect(entry.links[2].type).toBe("application/atom+xml");
-        expect(entry.links[2].href).toBe("/rest/api/product/1971/documents/opt");
+        expect(entry.links[2].href).toBe("/rest/api/product/1735/documents/opt?patient=false");
 
         expect(entry.links[3].rel).toBe("related");
         expect(entry.links[3].type).toBe("application/atom+xml");
-        expect(entry.links[3].href).toBe("/rest/api/product/1971/documents");
+        expect(entry.links[3].href).toBe("/rest/api/product/1735/documents");
 
         expect(entry.links[4].rel).toBe("related");
         expect(entry.links[4].type).toBe("application/atom+xml");
-        expect(entry.links[4].href).toBe("/rest/api/vmp/8028");
+        expect(entry.links[4].href).toBe("/rest/api/vmp/2675");
 
         expect(entry.category).not.toBe(null);
         expect(entry.category.term).toBe("PRODUCT");
         expect(entry.author.name.innerText).toBe("VIDAL");
-        expect(entry.id.innerText).toBe("vidal://product/1971");
-        expect(entry.updated.innerText).toBe("2014-09-17T22:00:00Z");
+        expect(entry.id.innerText).toBe("vidal://product/1735");
+        expect(entry.updated.innerText).toBe("2048-07-21T22:00:00Z");
         expect(entry.summary.type).toBe("text");
-        expect(entry.summary.innerText).toBe("BEDELIX 3 g pdre p susp buv");
-        expect(entry.vidal.id.innerText).toBe("1971");
+        expect(entry.summary.innerText).toBe("AUGMENTIN 100 mg/12,5 mg p ml pdre p susp buv Enf");
+        expect(entry.vidal.id.innerText).toBe("1735");
         expect(entry.vidal.dispensationplace.name).toBe("PHARMACY");
         expect(entry.vidal.dispensationplace.innerText).toBe("PHARMACY");
-        expect(entry.vidal.activeprinciples.innerText).toBe("montmorillonite beidellitique");
+        expect(entry.vidal.activeprinciples.innerText).toBe("acide clavulanique sel de K; amoxicilline trihydrate");
         expect(entry.vidal.horsghs.innerText).toBe("false");
-        expect(entry.vidal.refundrate.name).toBe("NR");
-        expect(entry.vidal.refundrate.innerText).toBe("NR");
+        expect(entry.vidal.refundrate.name).toBe("_65");
+        expect(entry.vidal.refundrate.innerText).toBe("65%");
         expect(entry.vidal.company.type).toBe("OWNER");
-        expect(entry.vidal.company.vidalid).toBe("2908");
-        expect(entry.vidal.company.innerText).toBe("Ipsen Pharma");
+        expect(entry.vidal.company.vidalid).toBe("986");
+        expect(entry.vidal.company.innerText).toBe("GlaxoSmithKline");
+        expect(entry.vidal.druginsport.innerText).toBe("false");
+        expect(entry.vidal.exceptional.innerText).toBe("false");
+        expect(entry.vidal.retrocession.innerText).toBe("false");
+        expect(entry.vidal.becareful.innerText).toBe("false");
+        expect(entry.vidal.midwife.innerText).toBe("false");
+        expect(entry.vidal.pervolume.innerText).toBe("100mg/12,5mgpml");
+        expect(entry.vidal.haspublisheddoc.innerText).toBe("true");
+        expect(entry.vidal.onmarketdate.format).toBe("yyyy-MM-dd");
+        expect(entry.vidal.onmarketdate.innerText).toBe("1998-05-14");
+        expect(entry.vidal.withoutprescription.innerText).toBe("false");
+        expect(entry.vidal.vmp.vidalid).toBe("2675");
+        expect(entry.vidal.vmp.innerText).toBe("acide clavulanique (sel de K) * 12,5 mg/ml + amoxicilline * 100 mg/ml ; voie orale ; pdre p susp buv");
     });
 
   });
